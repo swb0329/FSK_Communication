8bd8c0149aa5ed02183d2b5a600ab7227cdc5f5c
lin-jj <32064531+lin-jj@users.noreply.github.com>
2018-11-18 11:30:12 +0800

    Update fsk_modulator.v

--- fsk_modulator.v
+++ fsk_modulator.v
@@ -2,16 +2,22 @@ module	fsk_modulator(signal_out,signal_in,sysclk);
 	output	signal_out;
 	input	signal_in,sysclk;
 	
-	reg signal_out=0;
+	reg signal_out=1,enable=0;
 	wire clk_low,clk_high;
 
 	BaudRateGenerator brg(clk_low,clk_high,sysclk);
 
 	always@(posedge sysclk)
 	begin
-		if(signal_in==1)
-			signal_out<=clk_high;
-		else
-			signal_out<=clk_low;
+		if(signal_in==0&&enable==0)
+			enable<=1;
+		if(enable==1)
+		begin	
+			if(signal_in==1)
+				signal_out<=clk_high;
+			else
+				signal_out<=clk_low;
+		end
 	end
+
 endmodule
